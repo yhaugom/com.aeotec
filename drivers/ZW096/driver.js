f28935575e3008f810ba86471d9d163ccf0d3c5f
Robin Bolscher <robinbolscher@gmail.com>
2016-08-03 12:37:22 +0200

    More code style fixes

--- drivers/ZW096/driver.js
+++ drivers/ZW096/driver.js
@@ -40,89 +40,57 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		current_overload_protection: {
 			index: 3,
 			size: 1,
-			parser: input => {
-				console.log('Current overload protection: ', Number(input));
-				return new Buffer([Number(input)]);
-			}
+			parser: input => new Buffer([Number(input)])
 		},
 		output_load_status: {
 			index: 20,
 			size: 1,
-			parser: input => {
-				console.log('Output load status: ', Number(input));
-				return new Buffer([Number(input)]);
-			}
+			parser: input => new Buffer([Number(input)])
 		},
 		notify_associated_devices: {
 			index: 80,
 			size: 1,
-			parser: input => {
-				console.log('Notify associated devices: ', (input) ? 2 : 0);
-				return new Buffer([(input) ? 2 : 0]);
-			}
+			parser: input => new Buffer([(input) ? 2 : 0])
 		},
 		configure_led_state: {
 			index: 81,
 			size: 1,
-			parser: input => {
-				console.log('Configure LED state: ', Number(input));
-				return new Buffer([Number(input)]);
-			}
+			parser: input => new Buffer([Number(input)])
 		},
 		min_watt_change: {
 			index: 91,
 			size: 4,
-			parser: input => {
-				console.log('Min Watt change: ', input);
-				return new Buffer([input]);
-			}
+			parser: input => new Buffer([input])
 		},
 		report_group_1: {
 			index: 101,
 			size: 4,
-			parser: input => {
-				console.log('Report group 1: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
+			parser: input => new Buffer([0, 0, 0, Number(input)])
 		},
 		report_group_2: {
 			index: 102,
 			size: 4,
-			parser: input => {
-				console.log('Report group 2: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
+			parser: input => new Buffer([0, 0, 0, Number(input)])
 		},
 		report_group_3: {
 			index: 103,
 			size: 4,
-			parser: input => {
-				console.log('Report group 3: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
+			parser: input => new Buffer([0, 0, 0, Number(input)])
 		}, time_interval_1: {
 			index: 111,
 			size: 4,
-			parser: input => {
-				console.log('Time interval 1: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
+			parser: input => new Buffer([0, 0, 0, Number(input)])
 		},
 		time_interval_2: {
 			index: 112,
 			size: 4,
-			parser: input => {
-				console.log('Time interval 2: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
+			parser: input => new Buffer([0, 0, 0, Number(input)])
 		},
 		time_interval_3: {
 			index: 113,
 			size: 4,
-			parser: input => {
-				console.log('Time interval 3: ', Number(input));
-				return new Buffer([0, 0, 0, Number(input)]);
-			}
-		},
+			parser: input => new Buffer([0, 0, 0, Number(input)])
+
+		}
 	}
 });
