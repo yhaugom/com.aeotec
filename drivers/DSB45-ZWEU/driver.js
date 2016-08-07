6bb17da0deaef156264c3c0c7680e3ccc1f233b2
daneedk <daneedekruyff@outlook.com>
2016-08-07 09:20:53 +0200

    Added Water Sensor support (DSB45-ZWEU)

---
+++ drivers/DSB45-ZWEU/driver.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
+
+// http://products.z-wavealliance.org/products/1003
+// http://www.pepper1.net/zwavedb/device/637
+
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	capabilities: {
+		alarm_water: {
+			command_class: 'COMMAND_CLASS_SENSOR_BINARY', 
+ 			command_get: 'SENSOR_BINARY_GET', 
+ 			command_report: 'SENSOR_BINARY_REPORT',
+ 			command_report_parser: report => {
+				 if( report['Sensor Value'] !== 'detected an event') return false;
+				 	return true;
+			 }
+ 		},
+		measure_battery: {
+			command_class: 'COMMAND_CLASS_BATTERY',
+			command_get: 'BATTERY_GET',
+			command_report: 'BATTERY_REPORT',
+			command_report_parser: report => {
+				if (report['Battery Level'] === 'battery low warning') return 1;
+				return report['Battery Level (Raw)'][0];
+			}
+		}
+	},
+	settings: {
+		sensor_binary_report: {
+			index: 1,
+			size: 1,
+			parser: input => new Buffer([parseInt(input)])
+		},
+		Reports_that_will_be_sent: {
+			index: 121,
+			size: 4,
+			parser: input => new Buffer([0, 0, 0, Number(input)])
+		}
+	}
+});
