cad74c07fdcd53060b944f2ba4bfccbcafbdc892
Emile Nijssen <emile@athom.nl>
2016-10-07 19:55:00 +0200

    add support for Keyfob (Homey 0.10.4+) fixes #12 #13

--- drivers/ZW088/driver.js
+++ drivers/ZW088/driver.js
@@ -15,11 +15,31 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				return report['Battery Level (Raw)'][0];
 			}
 		}
-	},
-	settings: {
-		mode_control: {
-			index: 250,
-			size: 1
-		}
+	}
+});
+
+module.exports.on('initNode', function (token) {
+	let node = module.exports.nodes[token];
+	if( node ) {
+		node.instance.CommandClass['COMMAND_CLASS_SCENE_ACTIVATION'].on('report', (command, report) => {
+			if (command.name === 'SCENE_ACTIVATION_SET') {
+
+				var triggerMap = {
+					'1': 'press_1',
+					'2': 'hold_1',
+					'3': 'press_2',
+					'4': 'hold_2',
+					'5': 'press_3',
+					'6': 'hold_3',
+					'7': 'press_4',
+					'8': 'hold_4'
+				}
+
+				var triggerId = triggerMap[ report['Scene ID'] ];
+				if( triggerId ) {
+					Homey.manager('flow').triggerDevice(`zw088_${triggerId}`, null, null, node.device_data);
+				}
+			}
+		});
 	}
 });
