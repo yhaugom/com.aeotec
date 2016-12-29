1358a698976d41458da34d65fb7cdc4c2cdf142a
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 16:41:54 +0100

    ZW089: code cleanup

--- drivers/ZW089/driver.js
+++ drivers/ZW089/driver.js
@@ -13,26 +13,25 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_report: 'SENSOR_BINARY_REPORT',
 			command_report_parser: report => report['Sensor Value'] === 'detected an event'
 		},
-		
 		measure_battery: {
 			command_class: 'COMMAND_CLASS_BATTERY',
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
 			command_report_parser: report => {
 				if (report['Battery Level'] === 'battery low warning') return 1;
-				
-				return report['Battery Level (Raw)'][0];
-			}
-		}
+				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
+				return null;
+			},
+		},
 	},
 	settings: {
 		1: {
 			index: 1,
-			size: 1
+			size: 1,
 		},
 		101: {
 			index: 101,
 			size: 1,
 		},
-	}
+	},
 });
