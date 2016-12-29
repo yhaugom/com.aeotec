4425085b2f0dc13308f78fe03dca7c1429527759
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 16:02:30 +0100

    DSD37: code style

--- drivers/DSD37/driver.js
+++ drivers/DSD37/driver.js
@@ -6,8 +6,6 @@ const ZwaveDriver = require('homey-zwavedriver');
 // http://aeotec.com/z-wave-repeater
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-	capabilities: {
-	},
-	settings: {
-	}
+	capabilities: {},
+	settings: {},
 });
