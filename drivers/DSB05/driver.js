3233566a6981ad24183a68dc88f4a90787c329c1
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 16:31:02 +0100

    DSB05: check

--- drivers/DSB05/driver.js
+++ drivers/DSB05/driver.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
 
-// https://www.google.nl/url?sa=t&rct=j&q=&esrc=s&source=web&cd=9&cad=rja&uact=8&ved=0ahUKEwiWgsTGvZnRAhWBdlAKHd0uDkYQFghEMAg&url=http%3A%2F%2Fwww.zwaveproducts.com%2Fproduct-documentation%2FDSB05106-ZWUS.pdf&usg=AFQjCNGbYiR8fhq43xttq_4uJslQad0_Qg
+// http://www.cd-jackson.com/zwave_device_uploads/71/25-MultiSensor.pdf
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
@@ -11,7 +11,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
 			command_report_parser: report => {
-				if (report['Battery Level'] === "battery low warning") return 1;
+				if (report['Battery Level'] === 'battery low warning') return 1;
 				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
 				return null;
 			},
@@ -30,7 +30,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					if (report['Sensor Type'] === 'Temperature (version 1)') return report['Sensor Value (Parsed)'];
 				}
 				return null;
-			}
+			},
 		},
 		measure_luminance: {
 			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
@@ -50,45 +50,41 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					if (report['Sensor Type'] === 'Relative humidity (version 2)') return report['Sensor Value (Parsed)'];
 				}
 				return null;
-			}
-		}
+			},
+		},
 	},
 	settings: {
-		1: {
-			index: 1,
-			size: 1
-		},
 		2: {
 			index: 2,
-			size: 1
+			size: 1,
 		},
 		3: {
 			index: 3,
-			size: 2
+			size: 2,
 		},
 		4: {
 			index: 4,
-			size: 1
+			size: 1,
 		},
 		102: {
 			index: 102,
-			size: 4
+			size: 4,
 		},
 		103: {
 			index: 103,
-			size: 4
+			size: 4,
 		},
 		111: {
 			index: 111,
-			size: 4
+			size: 4,
 		},
 		112: {
 			index: 112,
-			size: 4
+			size: 4,
 		},
 		113: {
 			index: 113,
-			size: 4
-		}
-	}
+			size: 4,
+		},
+	},
 });
