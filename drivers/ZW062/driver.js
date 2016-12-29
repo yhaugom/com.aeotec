f8d26992d3a65071433b0fe355836cf0ffab9afd
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 16:09:51 +0100

    ZW062: code cleanup

--- drivers/ZW062/driver.js
+++ drivers/ZW062/driver.js
@@ -14,20 +14,18 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_report: 'BARRIER_OPERATOR_REPORT',
 			command_report_parser: report => report['State'] !== 'Closed',
 			command_set: 'BARRIER_OPERATOR_SET',
-			command_set_parser: input => {
-				return {
-					'Target Value': (input) ? 'OPEN' : 'CLOSE'
-				};
-			}
-		}
+			command_set_parser: input => ({
+				'Target Value': (input) ? 'OPEN' : 'CLOSE',
+			}),
+		},
 	},
 	settings: {
 		32: {
 			index: 32,
 			size: 1,
 		},
-		24: {
-			index: 24,
+		34: {
+			index: 34,
 			size: 1,
 		},
 		371: {
@@ -37,8 +35,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(input),
 				Number(newSettings[372]),
 				Number(newSettings[373]),
-				Number(newSettings[374])
-			])
+				Number(newSettings[374]),
+			]),
 		},
 		372: {
 			index: 37,
@@ -47,8 +45,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[371]),
 				Number(input),
 				Number(newSettings[373]),
-				Number(newSettings[374])
-			])
+				Number(newSettings[374]),
+			]),
 		},
 		373: {
 			index: 37,
@@ -57,8 +55,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[371]),
 				Number(newSettings[372]),
 				Number(input),
-				Number(newSettings[374])
-			])
+				Number(newSettings[374]),
+			]),
 		},
 		374: {
 			index: 37,
@@ -67,8 +65,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[371]),
 				Number(newSettings[372]),
 				Number(newSettings[373]),
-				Number(input)
-			])
+				Number(input),
+			]),
 		},
 		381: {
 			index: 38,
@@ -77,8 +75,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(input),
 				Number(newSettings[382]),
 				Number(newSettings[383]),
-				Number(newSettings[384])
-			])
+				Number(newSettings[384]),
+			]),
 		},
 		382: {
 			index: 38,
@@ -87,8 +85,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[381]),
 				Number(input),
 				Number(newSettings[383]),
-				Number(newSettings[384])
-			])
+				Number(newSettings[384]),
+			]),
 		},
 		383: {
 			index: 38,
@@ -97,8 +95,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[381]),
 				Number(newSettings[382]),
 				Number(input),
-				Number(newSettings[384])
-			])
+				Number(newSettings[384]),
+			]),
 		},
 		384: {
 			index: 38,
@@ -107,8 +105,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[381]),
 				Number(newSettings[382]),
 				Number(newSettings[383]),
-				Number(input)
-			])
+				Number(input),
+			]),
 		},
 		391: {
 			index: 39,
@@ -117,8 +115,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(input),
 				Number(newSettings[392]),
 				Number(newSettings[393]),
-				Number(newSettings[394])
-			])
+				Number(newSettings[394]),
+			]),
 		},
 		392: {
 			index: 39,
@@ -127,8 +125,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[391]),
 				Number(input),
 				Number(newSettings[393]),
-				Number(newSettings[394])
-			])
+				Number(newSettings[394]),
+			]),
 		},
 		393: {
 			index: 39,
@@ -137,8 +135,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[391]),
 				Number(newSettings[392]),
 				Number(input),
-				Number(newSettings[394])
-			])
+				Number(newSettings[394]),
+			]),
 		},
 		394: {
 			index: 39,
@@ -147,8 +145,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[391]),
 				Number(newSettings[392]),
 				Number(newSettings[393]),
-				Number(input)
-			])
+				Number(input),
+			]),
 		},
 		401: {
 			index: 40,
@@ -157,8 +155,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(input),
 				Number(newSettings[402]),
 				Number(newSettings[403]),
-				Number(newSettings[404])
-			])
+				Number(newSettings[404]),
+			]),
 		},
 		402: {
 			index: 40,
@@ -167,8 +165,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[401]),
 				Number(input),
 				Number(newSettings[403]),
-				Number(newSettings[404])
-			])
+				Number(newSettings[404]),
+			]),
 		},
 		403: {
 			index: 40,
@@ -177,8 +175,8 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[401]),
 				Number(newSettings[402]),
 				Number(input),
-				Number(newSettings[404])
-			])
+				Number(newSettings[404]),
+			]),
 		},
 		404: {
 			index: 40,
@@ -187,12 +185,12 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				Number(newSettings[401]),
 				Number(newSettings[402]),
 				Number(newSettings[403]),
-				Number(input)
-			])
+				Number(input),
+			]),
 		},
 		80: {
 			index: 80,
-			size: 1
+			size: 1,
 		},
 		241: {
 			index: 241,
@@ -200,7 +198,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			parser: input => {
 				if (input) return new Buffer([85, 85, 85, 1]);
 				return new Buffer([0, 0, 0, 0]);
-			}
-		}
-	}
+			},
+		},
+	},
 });
