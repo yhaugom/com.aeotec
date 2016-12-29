5c75c164f53517f2b8c6b627d403e481f3ddb094
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 16:39:55 +0100

    ZW088: code cleanup

--- drivers/ZW088/driver.js
+++ drivers/ZW088/driver.js
@@ -12,34 +12,33 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
 			command_report_parser: report => {
-				return report['Battery Level (Raw)'][0];
+				if (report['Battery Level'] === 'battery low warning') return 1;
+				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
+				return null;
 			},
-			pollInterval: 'poll_interval'
+			pollInterval: 'poll_interval',
 		}
 	}
 });
 
-module.exports.on('initNode', function (token) {
-	let node = module.exports.nodes[token];
+const triggerMap = {
+	1: 'press_1',
+	2: 'hold_1',
+	3: 'press_2',
+	4: 'hold_2',
+	5: 'press_3',
+	6: 'hold_3',
+	7: 'press_4',
+	8: 'hold_4',
+};
+
+module.exports.on('initNode', token => {
+	const node = module.exports.nodes[token];
 	if (node) {
 		node.instance.CommandClass['COMMAND_CLASS_SCENE_ACTIVATION'].on('report', (command, report) => {
 			if (command.name === 'SCENE_ACTIVATION_SET') {
-
-				var triggerMap = {
-					'1': 'press_1',
-					'2': 'hold_1',
-					'3': 'press_2',
-					'4': 'hold_2',
-					'5': 'press_3',
-					'6': 'hold_3',
-					'7': 'press_4',
-					'8': 'hold_4'
-				}
-
-				var triggerId = triggerMap[ report['Scene ID'] ];
-				if (triggerId) {
-					Homey.manager('flow').triggerDevice(`zw088_${triggerId}`, null, null, node.device_data);
-				}
+				const triggerId = triggerMap[report['Scene ID']];
+				if (triggerId) Homey.manager('flow').triggerDevice(`zw088_${triggerId}`, null, null, node.device_data);
 			}
 		});
 	}
