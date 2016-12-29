a2fab107bc9d4d33a224f85e0ebc1aa1ac1a9b2f
Robin Bolscher <robinbolscher@gmail.com>
2016-12-29 17:01:19 +0100

    ZW099: code cleanup

--- drivers/ZW099/driver.js
+++ drivers/ZW099/driver.js
@@ -11,27 +11,22 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			{
 				command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
 				command_set: 'SWITCH_MULTILEVEL_SET',
-				command_set_parser: value => {
-					return {
-						'Value': (value) ? 'on/enable' : 'off/disable',
-						'Dimming Duration': 255
-					};
-				}
+				command_set_parser: value => ({
+					'Value': (value) ? 'on/enable' : 'off/disable',
+					'Dimming Duration': 255,
+				}),
 			},
 			{
 				command_class: 'COMMAND_CLASS_BASIC',
 				command_get: 'BASIC_GET',
 				command_report: 'BASIC_REPORT',
 				command_report_parser: report => {
-					if (report.hasOwnProperty('Current Value'))
-						return report['Current Value'] !== 0;
-
-					if (report.hasOwnProperty('Value'))
-						return report['Value'] !== 0;
-				}
-			}
+					if (report.hasOwnProperty('Current Value')) return report['Current Value'] !== 0;
+					if (report.hasOwnProperty('Value')) return report['Value'] !== 0;
+					return null;
+				},
+			},
 		],
-
 		dim: [
 			{
 				command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
@@ -41,9 +36,9 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 
 					return {
 						'Value': value * 100,
-						'Dimming Duration': 255
-					}
-				}
+						'Dimming Duration': 255,
+					};
+				},
 			},
 			{
 				command_class: 'COMMAND_CLASS_BASIC',
@@ -56,53 +51,48 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					if (report.hasOwnProperty('Value')) {
 						return report['Value'] / 100;
 					}
-				}
-			}
+					return null;
+				},
+			},
 		],
-
 		measure_power: {
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser: () => {
-				return {
-					'Sensor Type': 'Electric meter',
-					'Properties1': {
-						'Scale': 0
-					}
-				}
-			},
+			command_get_parser: () => ({
+				'Sensor Type': 'Electric meter',
+				'Properties1': {
+					'Scale': 0,
+				},
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties1') &&
 					report.Properties1.hasOwnProperty('Scale') &&
-					report.Properties1['Scale'] === 0)
+					report.Properties1['Scale'] === 0) {
 					return report['Meter Value (Parsed)'];
-
+				}
 				return null;
-			}
+			},
 		},
-
 		meter_power: {
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser: () => {
-				return {
-					'Sensor Type': 'Electric meter',
-					'Properties1': {
-						'Scale': 2
-					}
-				}
-			},
+			command_get_parser: () => ({
+				'Sensor Type': 'Electric meter',
+				'Properties1': {
+					'Scale': 2,
+				},
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties1') &&
 					report.Properties1.hasOwnProperty('Scale') &&
-					report.Properties1['Scale'] === 2)
+					report.Properties1['Scale'] === 2) {
 					return report['Meter Value (Parsed)'];
-
+				}
 				return null;
-			}
-		}
+			},
+		},
 	},
 	settings: {
 		3: {
@@ -111,40 +101,40 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		20: {
 			index: 20,
-			size: 1
+			size: 1,
 		},
 		80: {
 			index: 80,
 			size: 1,
-			parser: input => new Buffer([(input) ? 2 : 0])
+			parser: input => new Buffer([(input) ? 2 : 0]),
 		},
 		81: {
 			index: 81,
-			size: 1
+			size: 1,
 		},
 		91: {
 			index: 91,
-			size: 4
+			size: 2,
 		},
 		102: {
 			index: 102,
-			size: 4
+			size: 4,
 		},
 		103: {
 			index: 103,
-			size: 4
+			size: 4,
 		},
 		111: {
 			index: 111,
-			size: 4
+			size: 4,
 		},
 		112: {
 			index: 112,
-			size: 4
+			size: 4,
 		},
 		113: {
 			index: 113,
-			size: 4
-		}
-	}
+			size: 4,
+		},
+	},
 });
