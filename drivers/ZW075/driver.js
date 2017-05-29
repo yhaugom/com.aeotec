c5c9ed76380fe2d81f3f203ed88fe24cf993e759
R Mengers <caseda08@hotmail.com>
2017-05-29 17:44:25 +0200

    ZW075: fix not updating state on startup (#59)
    * fix ZW075 not changing state on startup
    
    also the get "Scale" of measure and meter power were inverted.
    
    * a return too much
    
    * (re-)add checks to report parsers
    
    * swapped report parsers when fixing too big of a sync

--- drivers/ZW075/driver.js
+++ drivers/ZW075/driver.js
@@ -7,19 +7,30 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		onoff: [{
+		onoff: [
+			{
 				command_class: 'COMMAND_CLASS_SWITCH_BINARY',
+				command_get: 'SWITCH_BINARY_GET',
 				command_set: 'SWITCH_BINARY_SET',
 				command_set_parser: value => ({
-					'Switch Value': value,
+					'Switch Value': (value) ? 'on/enable' : 'off/disable',
 				}),
+				command_report: 'SWITCH_BINARY_REPORT',
+				command_report_parser: report => {
+					if (!report) return null;
+					if (typeof report.Value === 'string') return report.Value === 'on/enable';
+					if (typeof report.Value === 'number') return report.Value > 0;
+					if (typeof report['Current Value'] === 'number') return report['Current Value'] > 0;
+					return null;
+				},
 			},
 			{
 				command_class: 'COMMAND_CLASS_BASIC',
 				command_report: 'BASIC_REPORT',
 				command_report_parser: report => {
-					if (report.hasOwnProperty('Current Value')) return report['Current Value'] !== 0;
-					if (report.hasOwnProperty('Value')) return report['Value'] !== 0;
+					if (!report) return null;
+					if (typeof report.Value === 'string') return report.Value === 'on/enable';
+					if (typeof report.Value === 'number') return report.Value > 0;
 					return null;
 				},
 			},
@@ -28,14 +39,14 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
 			command_get_parser: () => ({
-				'Sensor Type': 'Electric meter',
-				'Properties1': {
-					'Scale': 0,
+				Properties1: {
+					Scale: 2,
 				},
 			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2') &&
+				if (report &&
+					report.hasOwnProperty('Properties2') &&
 					report.Properties2.hasOwnProperty('Scale bits 10') &&
 					report.Properties2['Scale bits 10'] === 2) {
 					return report['Meter Value (Parsed)'];
@@ -47,14 +58,14 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
 			command_get_parser: () => ({
-				'Sensor Type': 'Electric meter',
-				'Properties1': {
-					'Scale': 2,
+				Properties1: {
+					Scale: 0,
 				},
 			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2') &&
+				if (report &&
+					report.hasOwnProperty('Properties2') &&
 					report.Properties2.hasOwnProperty('Scale bits 10') &&
 					report.Properties2['Scale bits 10'] === 0) {
 					return report['Meter Value (Parsed)'];
