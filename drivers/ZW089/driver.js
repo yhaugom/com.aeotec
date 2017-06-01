f1d0fb7596a4b0a8f51365948062fb37d4336f39
R Mengers <caseda08@hotmail.com>
2017-06-01 12:20:57 +0200

    ZW089: remove wakeup interval, get battery on wakeup (#60)
    * remove wakeup interval, get battery on wakeup
    
    the wakeup interval in the app.json is not using the steps it must use,
    so wasn't a valid value anyway (this will revert it back to the default
    in the device then anyway)
    but wakeing up once a day (default) is more then enough.
    
    and battery level isn't reported on an interval, so getting it when it
    wakes up.
    
    also removed the get for binary sensor... as the device is asleep 99% of
    the time the app starts. saves traffic.
    
    * Fix missing report

--- drivers/ZW089/driver.js
+++ drivers/ZW089/driver.js
@@ -9,15 +9,19 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
 		alarm_contact: {
 			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
-			command_get: 'SENSOR_BINARY_GET',
 			command_report: 'SENSOR_BINARY_REPORT',
-			command_report_parser: report => report['Sensor Value'] === 'detected an event'
+			command_report_parser: report => {
+				if (report && report.hasOwnProperty('Sensor Value')) return report['Sensor Value'] === 'detected an event';
+				return null;
+			}
 		},
 		measure_battery: {
+			getOnWakeUp: true,
 			command_class: 'COMMAND_CLASS_BATTERY',
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
 			command_report_parser: report => {
+				if (!report) return null;
 				if (report['Battery Level'] === 'battery low warning') return 1;
 				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
 				return null;
