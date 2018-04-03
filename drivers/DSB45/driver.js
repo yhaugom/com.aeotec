19950d29957755cd926d25d28a6dc7f43c605af8
jolny <jolnyx@gmail.com>
2018-04-03 14:04:41 +0200

    Fixed support for DSB45 (different software versions?) (#103)

--- drivers/DSB45/driver.js
+++ drivers/DSB45/driver.js
@@ -8,15 +8,29 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		alarm_water: {
+		alarm_water: [{
 			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
 			command_get: 'SENSOR_BINARY_GET',
 			command_report: 'SENSOR_BINARY_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Sensor Value')) return report['Sensor Value'] === 'detected an event';
 				return null;
+			}
 			},
-		},
+			{
+			command_class: 'COMMAND_CLASS_BASIC',
+			command_set: 'BASIC_SET',
+			command_get: 'BASIC_GET',
+			command_report: 'BASIC_SET',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Value')) return report['Value'] !== 0;
+				if (report.hasOwnProperty('Value (Raw)')) {
+					return report['Value (Raw)'][0] !== 0;
+				}
+				return null;
+			}
+			},
+		],
 		measure_battery: {
 			command_class: 'COMMAND_CLASS_BATTERY',
 			command_get: 'BATTERY_GET',
@@ -38,4 +52,5 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			size: 4,
 		},
 	},
+	debug: false,
 });
